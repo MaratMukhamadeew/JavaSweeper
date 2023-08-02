0b2277ec83e81a3de9576233d259d0d00906993d
Marat Mukhamadeew <marat.mukhamadeew@yandex.ru>
2023-08-03 00:25:05 +0500

    Add override method - paintComponent

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -2,19 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JavaSweeper extends JFrame {
-    JPanel panel;
+    private JPanel panel;
+
     public static void main(String[] args) {
         new JavaSweeper();
     }
 
-    public JavaSweeper() throws HeadlessException {
-        panel = new JPanel();
+    private JavaSweeper() throws HeadlessException {
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawLine(0,0,500,300);
+            }
+        };
+
         panel.setPreferredSize(new Dimension(500, 300));
         add(panel);
+    }
+
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");
-        setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
     }
 }
